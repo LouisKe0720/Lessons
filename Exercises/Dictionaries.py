bb9896c7d273ccc244932dc6c714d83332d8c6a4
Louis0720 <Louiske720@gmail.com>
2024-11-14 11:19:15 -0500

    s

--- Exercises/Dictionaries.py
+++ Exercises/Dictionaries.py
@@ -8,12 +8,16 @@ favorite_colors = {
 # Print the dictionary
 print(favorite_colors)
 
+# Add a new person and their favorite color
 favorite_colors["David"] = "Yellow"
 
+# Update Kabi's favorite color
 favorite_colors["Kabi"] = "Purple"
 
+# Delete Someone in the dictionary
 del favorite_colors["Michelle"]
 
+# Check if someone is in the dictionary
 if "Michelle" in favorite_colors:
     print("Michelle is in the dictionary.")
 else:
